0e6dd6a4966353f5d84f1e43d2bc15c0759ef287
PabloNtn <pablo.ntn2020@gmail.com>
2021-02-24 11:05:32 -0300

    ate 20 min da aula 03
    Ja cadastrando as pesquisas de nota

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
-import { getCustomRepository, getRepository } from 'typeorm';
-import { User } from '../models/user';
+import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UserRepository';
 
 class UserController {
@@ -30,4 +29,4 @@ class UserController {
     }
 }
 
-export { UserController }
+export { UserController };
--- src/models/survey.ts
+++ src/models/survey.ts
@@ -1,16 +1,17 @@
-import { Column, PrimaryColumn, Entity, CreateDateColumn } from 'typeorm';
-import { v4 as uuid } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
 
-@Entity("users")
-class User {
+@Entity("surveys")
+class Survey {
+
     @PrimaryColumn()
     readonly id: string;
 
     @Column()
-    name: string;
+    titulo: string;
 
     @Column()
-    email: string;
+    description: string;
 
     @CreateDateColumn()
     created_at: Date;
@@ -22,5 +23,4 @@ class User {
     }
 }
 
-
-export { User };
+export { Survey };

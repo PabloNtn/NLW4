c05c7b00ed11c527c9ff95aff14b1967252968bc
PabloNtn <pablo.ntn2020@gmail.com>
2021-02-24 09:59:46 -0300

    primeiro commit

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { getCustomRepository, getRepository } from 'typeorm';
+import { User } from '../models/user';
+import { UsersRepository } from '../repositories/UserRepository';
+
+class UserController {
+
+    async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                error: "Usuario ja existe!",
+            })
+        }
+
+        const user = usersRepository.create({
+            name, email
+        })
+
+        await usersRepository.save(user);
+
+        return response.json(user);
+    }
+}
+
+export { UserController }

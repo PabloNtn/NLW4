af9a4dcc53522bd93fec5121edab8f30621e615e
PabloNtn <pablo.ntn2020@gmail.com>
2021-03-11 16:04:57 -0300

    aula 5 42min

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysUserRepository } from "../repositories/SurveyUserRepository";
 
 
 class AnswerController {
-    async execute(request: Request, repsonse: Response) {
+    async execute(request: Request, response: Response) {
+        const { value } = request.params;
+        const { u } = request.query;
 
+
+        const surveyUsersRepository = getCustomRepository(SurveysUserRepository);
+        const surveyUser = await surveyUsersRepository.findOne({
+            id: String(u)
+        });
+
+        if (!surveyUser) {
+            return response.status(400).json({
+                error: "Survey user does not exists!"
+            });
+        }
+
+        surveyUser.value = Number(value);
+
+        await surveyUsersRepository.save(surveyUser);
+
+        return response.json(surveyUser)
     }
 }
 
